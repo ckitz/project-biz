6a2caa8c94ea8969d5426043d263fa52ab7d3ab5
MattCordier <mcordier24@gmail.com>
2015-10-30 00:50:36 -0500

    style, script, and markup for products page with lightbox

--- script.js
+++ script.js
@@ -12,6 +12,61 @@ $("#reset2").click(function() {
 
 $(document).ready(function(){
     $("form").submit(function(){
-        alert("Thank you for your feedback");
+        alert("Thank you for contact us!");
     });
 });
+
+
+// LIGHTBOX
+
+jQuery(document).ready(function($) {
+	
+	$('.lightbox_trigger').click(function(e) {
+		
+		//prevent default action (hyperlink)
+		e.preventDefault();
+		
+		//Get clicked link src
+		var image_src = $(this).attr("src");
+		
+		/* 	
+		If the lightbox window HTML already exists in document, 
+		change the img src to to match the href of whatever link was clicked
+		
+		If the lightbox window HTML doesn't exists, create it and insert it.
+		(This will only happen the first time around)
+		*/
+		
+		if ($('#lightbox').length > 0) { // #lightbox exists
+			
+			//place href as img src value
+			$('#content').html('<img src="' + image_src + '" />');
+		   	
+			//show lightbox window - you could use .show('fast') for a transition
+			$('#lightbox').show();
+		}
+		
+		else { //#lightbox does not exist - create and insert (runs 1st time only)
+			
+			//create HTML markup for lightbox window
+			var lightbox = 
+			'<div id="lightbox">' +
+				'<p>Click to close</p>' +
+				'<div id="content">' + //insert clicked link's href into img src
+					'<img src="' + image_src +'" />' +
+				'</div>' +	
+			'</div>';
+				
+			//insert lightbox HTML into page
+			$('body').append(lightbox);
+		}
+		
+	});
+	
+	//Click anywhere on the page to get rid of lightbox window
+	$('body').on('click', '#lightbox' , function() {
+    $('#lightbox').hide();
+});
+
+});
+
